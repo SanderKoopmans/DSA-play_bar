ad46814043aad238361e82ef9b2f9db694b823fd
Sander <s.koopmans@ecmxperts.nl>
2021-06-20 22:19:28 +0200

    Setup basic state

--- src/progressBar/ProgressBar.jsx
+++ src/progressBar/ProgressBar.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './ProgressBar.css';
+
+const initialState = {
+  duration: 125368,
+  currentTime: 0,
+  playing: false
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'PLAY':
+      return {
+        ...state
+      }
+    case 'PAUSE': 
+      return {
+        ...state
+      }
+    default:
+      return console.log('No action selected');
+  }
+}
+
+const formatTime = (inputMilliSeconds) => {
+  const minutes = Math.floor(inputMilliSeconds / 60000);
+  const seconds = (( inputMilliSeconds % 60000) / 1000).toFixed(0);
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+}
 
 const ProgressBar = () => {
   return (
     <div className="bar-container">
       <div className="bar-top">
-        <button>Play</button>
+        {initialState.playing
+        ? <button>Pause</button>
+        : <button>Play</button>}
       </div>
       <div className="bar-bottom">
         <div className="progress-container"></div>
+        <div className="progress-duration">{formatTime(initialState.duration)}</div>
       </div>
 
     </div>

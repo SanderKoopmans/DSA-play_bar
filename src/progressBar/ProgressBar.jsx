314d4b98ed9e81de0d4365def5e417a8442077aa
Sander <s.koopmans@ecmxperts.nl>
2021-06-20 21:26:14 +0200

    init commit

---
+++ src/progressBar/ProgressBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProgressBar.css';
+
+const ProgressBar = () => {
+  return (
+    <div className="bar-container">
+      <p>Bar Container</p>
+    </div>
+  );
+};
+
+export default ProgressBar;
